7ea50be50877a05f831419ddc6b48db58c7e1d45
wildedcolin <wildedcolin@gmail.com>
2023-04-26 16:28:08 +0000

    i added a new file

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo i am working perfectly
+
+# End
