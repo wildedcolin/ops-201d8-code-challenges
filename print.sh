3de8c442a4005fc73a52026292acaee6bf35e776
wildedcolin <wildedcolin@gmail.com>
2023-04-26 22:09:27 +0000

    this is the print file

---
+++ print.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+function login_history {
+    
+echo "this is the login history"
+
+last
+}
+
+
+# Main
+
+login_history
+login_history
+login_history
+
+# End
