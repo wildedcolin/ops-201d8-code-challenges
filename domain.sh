8b1e9283d19d2d2a5fa1f18d3d29c393a7a0410e
wildedcolin <wildedcolin@gmail.com>
2023-05-11 18:04:14 +0000

    this is it

--- domain.sh
+++ domain.sh
@@ -1,27 +1,103 @@
 #!/bin/bash
 
-# Take user input
-read -p "Enter a domain name: " domain
+echo "Enter domain"
+read var1
 
-# Run whois
-echo "Running whois for $domain..."
-whois "$domain" > "$domain.txt"
+whois $var1 >> domain.sh
+dig $var1 >> domain.sh
+host $var1 >> domain.sh
+nslookup $var >> domain.sh
 
-# Run dig
-echo "Running dig for $domain..."
-dig "$domain" >> "$domain.txt"
+; <<>> DiG 9.18.12-0ubuntu0.22.04.1-Ubuntu <<>> www.google.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 23817
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
 
-# Run host
-echo "Running host for $domain..."
-host "$domain" >> "$domain.txt"
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 65494
+;; QUESTION SECTION:
+;www.google.com.			IN	A
 
-# Run nslookup
-echo "Running nslookup for $domain..."
-nslookup "$domain" >> "$domain.txt"
+;; ANSWER SECTION:
+www.google.com.		128	IN	A	142.251.46.228
 
-# Display domain information from nslookup
-echo "Domain information for $domain:"
-nslookup -type=any "$domain"
+;; Query time: 12 msec
+;; SERVER: 127.0.0.53#53(127.0.0.53) (UDP)
+;; WHEN: Thu May 11 17:58:08 UTC 2023
+;; MSG SIZE  rcvd: 59
 
-# Output results to a file
-echo "Results saved to $domain.txt"
+www.google.com has address 142.251.46.228
+www.google.com has IPv6 address 2607:f8b0:4005:814::2004
+;; communications error to 127.0.0.53#53: timed out
+Server:		127.0.0.53
+Address:	127.0.0.53#53
+
+** server can't find sudo: SERVFAIL
+
+; <<>> DiG 9.18.12-0ubuntu0.22.04.1-Ubuntu <<>> www.google.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 55830
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
+
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 65494
+;; QUESTION SECTION:
+;www.google.com.			IN	A
+
+;; ANSWER SECTION:
+www.google.com.		72	IN	A	142.251.46.228
+
+;; Query time: 0 msec
+;; SERVER: 127.0.0.53#53(127.0.0.53) (UDP)
+;; WHEN: Thu May 11 17:59:04 UTC 2023
+;; MSG SIZE  rcvd: 59
+
+www.google.com has address 142.251.46.228
+www.google.com has IPv6 address 2607:f8b0:4005:814::2004
+
+; <<>> DiG 9.18.12-0ubuntu0.22.04.1-Ubuntu <<>> www.google.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 37858
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
+
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 65494
+;; QUESTION SECTION:
+;www.google.com.			IN	A
+
+;; ANSWER SECTION:
+www.google.com.		228	IN	A	142.251.214.132
+
+;; Query time: 0 msec
+;; SERVER: 127.0.0.53#53(127.0.0.53) (UDP)
+;; WHEN: Thu May 11 18:01:52 UTC 2023
+;; MSG SIZE  rcvd: 59
+
+www.google.com has address 142.251.214.132
+www.google.com has IPv6 address 2607:f8b0:4005:80b::2004
+
+
+; <<>> DiG 9.18.12-0ubuntu0.22.04.1-Ubuntu <<>> www.google.com
+;; global options: +cmd
+;; Got answer:
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 6685
+;; flags: qr rd ra; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1
+
+;; OPT PSEUDOSECTION:
+; EDNS: version: 0, flags:; udp: 65494
+;; QUESTION SECTION:
+;www.google.com.			IN	A
+
+;; ANSWER SECTION:
+www.google.com.		131	IN	A	142.251.214.132
+
+;; Query time: 0 msec
+;; SERVER: 127.0.0.53#53(127.0.0.53) (UDP)
+;; WHEN: Thu May 11 18:03:29 UTC 2023
+;; MSG SIZE  rcvd: 59
+
+www.google.com has address 142.251.214.132
+www.google.com has IPv6 address 2607:f8b0:4005:810::2004
